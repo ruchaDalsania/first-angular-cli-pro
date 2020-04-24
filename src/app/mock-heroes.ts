e1a0b53a38aae990ef3c64628d7fee55d8324b2e
Rucha Dalsania <dalsania.rucha@yahoo.co.in>
2020-04-24 16:03:55 -0400

    initial working project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[]=[
+	{id:11, name:"Thor"},
+	{id:12, name:"Great-Hulk"},
+	{id:13, name:"Tony-Stark"},
+	{id:14, name:"Dr-Strange"},
+	{id:15, name:"Captain-Marvel"}
+];
